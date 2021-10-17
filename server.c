823c1589116652b3d200c2b96b8002dfda3c271f
Miguel <miguel.perezf@upb.edu.co>
2021-10-17 15:05:28 -0500

    prueba shared memory

--- server.c
+++ server.c
@@ -1,10 +1,19 @@
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
 #include <pthread.h>
 #include <termios.h>
+//#include <sys/ipc.h>
+//#include <sys/msg.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <semaphore.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+#define SH_SIZE 16
 
 void CmExit(char *buffer);
 void CmAdd(char *buffer);
@@ -12,17 +21,47 @@ void CmRemove(char *buffer);
 void CmTrigger(char *buffer);
 void CmList(char *buffer);
 
-void *hilolecturafifo(void *param);
+//void *hilolecturamsg(void *param);
 
 // Declaracion de variable que funcionara como switch de manera global
 int conv = 6;
 
 int main(int argc, char **argv)
 {
-    pthread_t threadID;
-    pthread_create(&threadID, NULL, &hilolecturafifo, NULL);
+    /*pthread_t threadID;
+    pthread_create(&threadID, NULL, &hilolecturamsg, NULL);*/
 
-    char respuesta[16];
+    int shm_fd = shm_open("/shm0", O_CREAT | O_RDWR, 0600);
+    if (shm_fd < 0)
+    {
+        perror("shm memory error: ");
+        exit(EXIT_FAILURE);
+    }
+    fprintf(stdout, "Shared memory is created with fd: %d\n", shm_fd);
+
+    if (ftruncate(shm_fd, SH_SIZE * sizeof(char)) < 0)
+    {
+        perror("Truncation failed: ");
+        exit(EXIT_FAILURE);
+    }
+
+    fprintf(stdout, "The memory region is truncated.\n");
+
+    void *map = mmap(NULL, SH_SIZE, PROT_WRITE, MAP_SHARED, shm_fd, 0);
+
+    if (map == MAP_FAILED)
+    {
+        perror("Mapping failed: ");
+        exit(EXIT_FAILURE);
+    }
+    char* ptr = (char*)map;
+     ptr[0] = 'A';
+     ptr[1] = 'B';
+     ptr[2] = 'C';
+     ptr[3] = '\n';
+     ptr[4] = '\0';
+
+    /*char respuesta[16];
     while (1)
     {
         scanf("%s", respuesta);
@@ -60,7 +99,7 @@ int main(int argc, char **argv)
             conv = 5;
         }
     }
-    return NULL;
+    return NULL;*/
 }
 void CmExit(char *buffer)
 {
@@ -82,7 +121,7 @@ void CmList(char *buffer)
 {
     //Proceso de cuando se pide list
 }
-void *hilolecturafifo(void *param) //Proceso hilo
+/*void *hilolecturamsg(void *param) //Proceso hilo
 {
     //Todo el proceso para leer el fifo
-}
+}*/
